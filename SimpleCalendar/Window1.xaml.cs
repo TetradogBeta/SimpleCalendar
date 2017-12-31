c2bfc3b55918a73ce83742a873285134fb1309d0
Gabriel <tetradog@gmail.com>
2018-01-01 00:28:55 +0100

    subir

---
+++ SimpleCalendar/Window1.xaml.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: tetradog
+ * Fecha: 01/01/2018
+ * Licencia GNU v3
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace SimpleCalendar
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class Window1 : Window
+	{
+		public Window1()
+		{
+			InitializeComponent();
+		}
+	}
+}
